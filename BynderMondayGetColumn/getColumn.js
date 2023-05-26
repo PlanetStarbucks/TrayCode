6d3ccbf57f71c7377e3cc4fc4cf2307fdcd9c048
PlanestStarbucks <wkeiffer@gmail.com>
2023-05-26 09:51:22 -0700

    Update to Board List

---
+++ BynderMondayGetColumn/getColumn.js
@@ -0,0 +1,74 @@
+tray.on("CONFIG_SLOT_MOUNT", async ({ event, previousWizardState }) => {
+	if (event.data.externalId === "external_columnid") {
+		console.log({ event, previousWizardState });
+		if (
+			previousWizardState.values.external_boardid === false ||
+			previousWizardState.values.external_boardid === null ||
+			previousWizardState.values.external_boardid === undefined
+		) {
+			return {
+				...event.data,
+				status: "LOADING",
+			};
+		} else {
+			const boardID = previousWizardState.values.external_boardid;
+			return await getJsonSchema({ event, previousWizardState, boardID });
+		}
+	} else {
+		return;
+	}
+});
+
+tray.on("CONFIG_SLOT_VALUE_CHANGED", async ({ event, previousWizardState }) => {
+	if (event.data.externalId === "external_boardid") {
+		console.log("getting jsonSchema");
+		const boardID = event.data.value;
+		return await getJsonSchema({ event, previousWizardState, boardID });
+	} else {
+		return;
+	}
+});
+
+async function getJsonSchema({ event, previousWizardState, boardID }) {
+	const response = await tray.callConnector({
+		connector: "graphql-client",
+		version: "1.1",
+		operation: "raw_query",
+		authId: previousWizardState.values.external_monday_authentication,
+		input: {
+			query: "query getColumns($boardID: [Int]!) {\n  boards (ids: $boardID) {\n\t\tcolumns {\n      id\n      title\n    }\n  }\n}",
+			graphql_endpoint: "https://api.monday.com/v2",
+			headers: [
+				{
+					key: "Authorization",
+					value: "{$.auth.api_key}",
+				},
+			],
+			variables: [
+				{
+					key: "boardID",
+					value: Number(boardID),
+				},
+			],
+		},
+	});
+
+	const mondayRes = JSON.parse(response);
+	console.log(response);
+	const columns = Object.entries(mondayRes.data.boards[0].columns).map((x) => {
+		return {
+			name: x[1].title,
+			value: x[1].id,
+		};
+	});
+
+	//return jsonSchema in { text, value } to render in dropdown.
+	return {
+		...event.data,
+		type: "string",
+		status: "VISIBLE",
+		jsonSchema: {
+			enum: columns.map(({ name, value }) => ({ text: name, value: value })),
+		},
+	};
+}
--- WebdamFolderPicklist/webdamUploadFolder.js
+++ WebdamFolderPicklist/webdamUploadFolder.js
@@ -1,161 +1,144 @@
 //What to do when the page initially load.  If there's a value, keep that value.
 //If not, then wait for an auth and then get the folders
-tray.on(
-  "CONFIG_SLOT_MOUNT",
-  async ({ event, previousWizardState, previousSlotState }) => {
-    if (event.data.externalId !== "external_uploadfolder") {
-      return;
-    } else {
-      if (
-        previousWizardState.values.external_uploadfolder === false ||
-        previousWizardState.values.external_uploadfolder === undefined ||
-        previousWizardState.values.external_uploadfolder === ""
-      ) {
-        console.log("No auth. Add a valid auth for Webdam");
-        return {
-          ...event.data,
-          status: "LOADING",
-        };
-      } else {
-        return await folderState({
-          event,
-          previousWizardState,
-          previousSlotState,
-        });
-      }
-    }
-  }
-);
+tray.on("CONFIG_SLOT_MOUNT", async ({ event, previousWizardState, previousSlotState }) => {
+	if (event.data.externalId !== "external_uploadfolder") {
+		return;
+	} else {
+		if (
+			previousWizardState.values.external_uploadfolder === false ||
+			previousWizardState.values.external_uploadfolder === undefined ||
+			previousWizardState.values.external_uploadfolder === ""
+		) {
+			console.log("No auth. Add a valid auth for Webdam");
+			return {
+				...event.data,
+				status: "LOADING",
+			};
+		} else {
+			return await folderState({
+				event,
+				previousWizardState,
+				previousSlotState,
+			});
+		}
+	}
+});
 
 //When an auth is submitted to the Webdam Auth slot, or is changed, get the folders
-tray.on(
-  "AUTH_SLOT_VALUE_CHANGED",
-  async ({ event, previousWizardState, previousSlotState }) => {
-    if (
-      previousWizardState.values.external_uploadfolder === false ||
-      previousWizardState.values.external_uploadfolder === undefined ||
-      previousWizardState.values.external_uploadfolder === ""
-    ) {
-      return {
-        ...event.data,
-        status: "VISIBLE",
-        jsonSchema: await getJsonSchema({ previousWizardState }),
-      };
-    } else {
-      return await folderState({
-        event,
-        previousWizardState,
-        previousSlotState,
-      });
-    }
-  }
-);
+tray.on("AUTH_SLOT_VALUE_CHANGED", async ({ event, previousWizardState, previousSlotState }) => {
+	if (
+		previousWizardState.values.external_uploadfolder === false ||
+		previousWizardState.values.external_uploadfolder === undefined ||
+		previousWizardState.values.external_uploadfolder === ""
+	) {
+		return {
+			...event.data,
+			status: "VISIBLE",
+			jsonSchema: await getJsonSchema({ previousWizardState }),
+		};
+	} else {
+		return await folderState({
+			event,
+			previousWizardState,
+			previousSlotState,
+		});
+	}
+});
 
 async function getJsonSchema({ previousWizardState }, uploadFolder) {
-  // use the Webdam connector to call the top level folder in account
+	// use the Webdam connector to call the top level folder in account
 
-  const response = await tray.callConnector({
-    connector: "webdam",
-    version: "1.0",
-    operation: "list_folders",
-    authId: previousWizardState.values.external_webdam_authentication,
-    input: {},
-  });
+	const response = await tray.callConnector({
+		connector: "webdam",
+		version: "1.0",
+		operation: "list_folders",
+		authId: previousWizardState.values.external_webdam_authentication,
+		input: {},
+	});
 
-  // parse response into a proper object
-  const folderArray = JSON.parse(response);
-  //return JSON object to generate dropdown in Tray config
-  return {
-    // output the list of fields to pick from in the data mapping component
-    enum: await enumerator(
-      folderArray.results,
-      previousWizardState,
-      uploadFolder
-    ),
-  };
+	// parse response into a proper object
+	const folderArray = JSON.parse(response);
+	//return JSON object to generate dropdown in Tray config
+	return {
+		// output the list of fields to pick from in the data mapping component
+		enum: await enumerator(folderArray.results, previousWizardState, uploadFolder),
+	};
 }
 
 //function to create an array for Tray to render.  The first value is the root folder value, which should prompt the workflow to create a new folder.
 const enumerator = async function (input, previousWizardState, uploadFolder) {
-  let newArray = [
-    /* commentting this out for now...need to figure out how to handle the different scenarios when a user updates the config
+	let newArray = [
+		/* commentting this out for now...need to figure out how to handle the different scenarios when a user updates the config
 			  {
 				  "text": "Create New Folder",
 				  "value": 0
 			  }
 					*/
-  ];
-  input.forEach((element) => {
-    newArray.push({
-      text: element.name,
-      value: element.id,
-    });
-  });
-  if (uploadFolder !== undefined) {
-    const folderDetail = await folderRes(previousWizardState);
-    newArray.push({
-      text: folderDetail.name,
-      value: folderDetail.id,
-    });
-  }
-  return newArray;
+	];
+	input.forEach((element) => {
+		newArray.push({
+			text: element.name,
+			value: element.id,
+		});
+	});
+	if (uploadFolder !== undefined) {
+		const folderDetail = await folderRes(previousWizardState);
+		newArray.push({
+			text: folderDetail.name,
+			value: folderDetail.id,
+		});
+	}
+	return newArray;
 };
 
 //function to call the Webdam folder status
 const folderRes = async function (previousWizardState) {
-  let res = "";
-  if (previousWizardState.values.external_uploadfolder === undefined) {
-    return {
-      message: "Not Found",
-    };
-  } else {
-    try {
-      res = await tray.callConnector({
-        connector: "webdam",
-        version: "1.2",
-        operation: "get_folder",
-        authId: previousWizardState.values.external_webdam_authentication,
-        input: {
-          folder_id: Number(previousWizardState.values.external_uploadfolder),
-        },
-      });
-    } catch (err) {
-      console.log("folder deleted, getting new folder list");
-      res = {
-        message: "Not Found",
-      };
-      return res;
-    }
-    const response = JSON.parse(res);
-    return response;
-  }
+	let res = "";
+	if (previousWizardState.values.external_uploadfolder === undefined) {
+		return {
+			message: "Not Found",
+		};
+	} else {
+		try {
+			res = await tray.callConnector({
+				connector: "webdam",
+				version: "1.2",
+				operation: "get_folder",
+				authId: previousWizardState.values.external_webdam_authentication,
+				input: {
+					folder_id: Number(previousWizardState.values.external_uploadfolder),
+				},
+			});
+		} catch (err) {
+			console.log("folder deleted, getting new folder list");
+			res = {
+				message: "Not Found",
+			};
+			return res;
+		}
+		const response = JSON.parse(res);
+		return response;
+	}
 };
 
-const folderState = async function ({
-  event,
-  previousWizardState,
-  previousSlotState,
-}) {
-  console.log("getting folder state");
-  const folderState = await folderRes(previousWizardState);
-  if (folderState.message === "Not Found") {
-    console.log("folder not found");
-    {
-      return {
-        ...event.data,
-        status: "VISIBLE",
-        jsonSchema: await getJsonSchema({ previousWizardState }),
-      };
-    }
-  } else {
-    console.log("folder found, returning previous value");
-    return {
-      ...previousSlotState,
-      status: "VISIBLE",
-      jsonSchema: await getJsonSchema(
-        { previousWizardState },
-        previousSlotState.value
-      ),
-    };
-  }
+const folderState = async function ({ event, previousWizardState, previousSlotState }) {
+	console.log("getting folder state");
+	const folderState = await folderRes(previousWizardState);
+	if (folderState.message === "Not Found") {
+		console.log("folder not found");
+		{
+			return {
+				...event.data,
+				status: "VISIBLE",
+				jsonSchema: await getJsonSchema({ previousWizardState }),
+			};
+		}
+	} else {
+		console.log("folder found, returning previous value");
+		return {
+			...previousSlotState,
+			status: "VISIBLE",
+			jsonSchema: await getJsonSchema({ previousWizardState }, previousSlotState.value),
+		};
+	}
 };
