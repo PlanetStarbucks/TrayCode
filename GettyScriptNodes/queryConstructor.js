fa034bff392a385f33a14358406173f04c081595
PlanestStarbucks <wkeiffer@gmail.com>
2023-07-05 10:01:19 -0700

    updated queryConstructor for Getty

---
+++ GettyScriptNodes/queryConstructor.js
@@ -0,0 +1,266 @@
+const input = {
+	metadataMap: [
+		{
+			key: "id",
+			value: "7FE02A94-6EC8-4D82-8976C422799BC0BC",
+		},
+		{
+			key: "city",
+			value: "C578B95C-A773-4D1A-B6F676BA5DA933F7",
+		},
+		{
+			key: "mp",
+			value: "28B8037E-A073-4677-9CCA2D44A6CFF1AB",
+		},
+		{
+			key: "caption",
+			value: "somthing",
+		},
+	],
+	staticMapInput: [
+		{
+			text: "Explicit Use",
+			value: "7B8012C9-5123-40F8-80577C00149BE0D7",
+		},
+		{
+			text: "Asset Sub-Type",
+			value: "ACC77C3C-F228-4DAF-B83791222F5B630F",
+		},
+		{
+			text: "Workfront Original Project",
+			value: "53557627-1062-4A48-82AA85E4EE45C2BB",
+		},
+		{
+			text: "Channel",
+			value: "3678354C-25C8-4F3E-BE6F31F51E3F5E93",
+		},
+		{
+			text: "Campaign IMC Categories",
+			value: "AFDE73B0-7998-4DF2-B5D5092DBD42D701",
+		},
+		{
+			text: "Customer Guardrails",
+			value: "AA36D926-C482-4E70-83C835DEBB47A3F4",
+		},
+		{
+			text: "Getty ID Number",
+			value: "7FE02A94-6EC8-4D82-8976C422799BC0BC",
+		},
+		{
+			text: "JIRA Ticket",
+			value: "864D3CB7-1DAA-4406-975DF75D0DA27FBD",
+		},
+		{
+			text: "Product",
+			value: "1EF24BCF-26BD-4BDC-98A4FB16B75244F7",
+		},
+		{
+			text: "Attribution Required Disclaimer",
+			value: "A5959C11-19E2-48C2-8A27BF57C8A3DD22",
+		},
+		{
+			text: "Campaign IMC Sub-Categories",
+			value: "89F1A1DE-8FBE-4CC9-9DEAA3471609B265",
+		},
+		{
+			text: "Figmage page id",
+			value: "757401F4-FD60-4CBE-8EF4870A40A63C41",
+		},
+		{
+			text: "Stock",
+			value: "DEEB2B07-9CC5-4C06-9339FDC14E60C31E",
+		},
+		{
+			text: "Visibility",
+			value: "F430A723-6466-4D45-81E642CB4FF15AF3",
+		},
+		{
+			text: "Region",
+			value: "3436E175-2362-4119-856B707B5EB2B7DD",
+		},
+		{
+			text: "Expiration",
+			value: "53E94255-B3D3-4493-8EE739B27E1126D5",
+		},
+		{
+			text: "GDDAM Asset ID",
+			value: "303A6695-0F59-44DE-9A4C901FF81E310A",
+		},
+		{
+			text: "File Type",
+			value: "9A79931D-3FD8-4483-A4E33B250DF4FD9E",
+		},
+		{
+			text: "Attribution Required",
+			value: "503A06B6-DE73-4AFE-95F004DDDFC5965A",
+		},
+		{
+			text: "Partner Brands",
+			value: "5513F237-9481-4B12-A8D5D64CA067C449",
+		},
+		{
+			text: "Workfront Search",
+			value: "496C6142-8336-4DB9-A6EE5F7A500EA97C",
+		},
+		{
+			text: "Figmage node id",
+			value: "1C8D3EFF-00C2-4BB8-B60836FCB8F07981",
+		},
+		{
+			text: "Website URL",
+			value: "6DCCA14C-6EAC-4076-BA692D768CED4E73",
+		},
+		{
+			text: "Figmage doc id",
+			value: "90BB75FF-2976-4A80-AD5A07E0A99BD95F",
+		},
+		{
+			text: "Language",
+			value: "041A6243-187C-4791-B451FDCC79E5B381",
+		},
+		{
+			text: "Asset Type",
+			value: "A7187D67-F8FF-413A-9F23EBB1AAC8C1A1",
+		},
+		{
+			text: "Country",
+			value: "0476F7B9-4355-455B-BBA4BD6C3DE073F2",
+		},
+		{
+			text: "Live Media",
+			value: "5B1D5926-D27E-445E-AC44844250FF1851",
+		},
+		{
+			text: "Customer",
+			value: "0C518565-C965-4B66-8F6EB5FF790DDA2C",
+		},
+		{
+			text: "Business Name",
+			value: "9402FA57-6DB6-4613-93FBED56CFF237FE",
+		},
+		{
+			text: "Related Assets",
+			value: "AA61DB49-EAE7-4A22-A149DB3B7B9851C3",
+		},
+		{
+			text: "Admin Notes",
+			value: "FDA0E300-1C0E-4F22-AABE5A2E4C0F364B",
+		},
+		{
+			text: "Location",
+			value: "C578B95C-A773-4D1A-B6F676BA5DA933F7",
+		},
+		{
+			text: "Subject",
+			value: "161F012E-7EDF-4EBD-98C8C537769C6F1B",
+		},
+		{
+			text: "Download Approver",
+			value: "389011EF-070D-46A3-91386BD243D75255",
+		},
+		{
+			text: "TLD",
+			value: "EBE10520-2B5B-4B1F-8A4E3158CAF0A8D3",
+		},
+		{
+			text: "Events",
+			value: "48987484-1DBE-42E0-9B455119DF290691",
+		},
+		{
+			text: "Acquired Brands",
+			value: "5DA92533-80EA-425E-9010DC7C9372A2DC",
+		},
+		{
+			text: "Brand",
+			value: "58ACAD82-BF8A-40D1-942761965541C422",
+		},
+		{
+			text: "Business Verticals",
+			value: "9DC9F210-C52A-474B-946C7E9F8A3D0AAA",
+		},
+		{
+			text: "Photographer",
+			value: "C240DB96-26A8-4A43-BF6F206F4CE8DC6B",
+		},
+		{
+			text: "seoText",
+			value: "7F873E8B-A195-45EA-9CA216EEDD92C33B",
+		},
+		{
+			text: "Figmage image extension",
+			value: "03DF5634-3BB5-41B2-AD137F7656D2DAF5",
+		},
+		{
+			text: "Getty - License Type",
+			value: "28B8037E-A073-4677-9CCA2D44A6CFF1AB",
+		},
+		{
+			text: "Program",
+			value: "EBFDADD2-B8C1-4F83-90CA8E12EDD883A9",
+		},
+	],
+	fullMapInput: {},
+};
+
+const run = function (input) {
+	//remove any null values from the metadata map.  This can happen when someone leaves an empty box in the mapping tool
+	const metadataMap = input.metadataMap.filter((element) => {
+		if (element.key === null || element.value === null) {
+			return;
+		} else {
+			return element;
+		}
+	});
+
+	//remove any nullvalues from the fullMapInput, and re-format it for the save asset call
+	Object.keys(input.fullMapInput).forEach((key) => {
+		if (key == "null") {
+			delete input.fullMapInput[key];
+		} else {
+			const newKey = `metaproperty.${key}`;
+			input.fullMapInput[newKey] = input.fullMapInput[key];
+			delete input.fullMapInput[key];
+		}
+	});
+
+	//create the base queryObject
+	const queryObject = {
+		id: "id",
+		keywords: "[keywords.name, id, 'Getty']",
+		title: "title",
+	};
+
+	metadataMap.forEach((element) => {
+		queryObject[element.key] = element.key;
+	});
+
+	//create transformer string from config values.  This is used in the compactify metadata step when querying assets from Getty.
+	let transformerString = `"id": id,"keywords": [keywords.text, id, 'Getty'],"title": title`;
+
+	metadataMap.forEach((element) => {
+		if (element.key === "id" || element.key === "title") {
+			//skip
+		} else {
+			transformerString = transformerString.concat(`,"`, element.key, `":`, element.key);
+		}
+	});
+
+	const transformerQueryString = `[response.images[$contains(license_model, "royaltyfree")].{` + transformerString + `}]`;
+
+	//create mapObject that will be used to merge metadata from assets into an object for the Bynder save asset call
+	let mapObject = {};
+	for (let i = 0; i < metadataMap.length; i++) {
+		const keyValue = `metaproperty.${metadataMap[i].value}`;
+		mapObject[keyValue] = metadataMap[i].key;
+	}
+
+	return {
+		transformerString: transformerQueryString,
+		queryObject,
+		mapObject,
+		staticMeta: input.fullMapInput,
+		metadataMap,
+	};
+};
+
+console.log(run(input));
